d4251e8b654c51e9be0d75a9fc58d1dd9400177b
César <cesarsantossouza99@gmail.com>
2023-03-13 19:28:21 -0300

    Adicionando um onClick

--- src/components/CardsProduct/index.js
+++ src/components/CardsProduct/index.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import { useCart } from '../../hooks/CartContentx'
 import { Button } from '../Button'
 import {
   Container,
@@ -10,13 +11,14 @@ import {
 } from '../CardsProduct/styles'
 
 export function CardProduct({ product }) {
+  const { putProductInCart } = useCart()
   return (
     <Container>
       <Image src={product.url} alt="Imagem do produto" />
       <div>
         <ProductName>{product.name}</ProductName>
         <ProductPrice>{product.formatedPrice}</ProductPrice>
-        <Button> Adicionar</Button>
+        <Button onClick={() => putProductInCart(product)}> Adicionar</Button>
       </div>
     </Container>
   )
