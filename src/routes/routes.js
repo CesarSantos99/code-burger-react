5e5829ad14f4a5f25617a85c207e0557cefeb7a3
César <cesarsantossouza99@gmail.com>
2023-03-14 14:59:44 -0300

    Rota do carrinho

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 
-import { Home, Login, Product, Register } from '../containers'
+import { Home, Login, Product, Register, Cart } from '../containers'
 import PrivateRoute from './private-route'
 
 function Routes() {
@@ -12,6 +12,7 @@ function Routes() {
         <Route component={Register} path="/Cadastro" />
         <PrivateRoute exact component={Home} path="/" />
         <PrivateRoute component={Product} path="/produtos" />
+        <PrivateRoute component={Cart} path="/carrinho" />
       </Switch>
     </Router>
   )
