f43f7fbc2f102f823811faec84d396145e22e937
César <cesarsantossouza99@gmail.com>
2023-03-15 17:19:37 -0300

    CartItems

--- src/containers/Cart/index.js
+++ src/containers/Cart/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import CartLogo from '../../assets/logo-carinho.svg'
+import { CartItems } from '../../components'
 import { Container, CartImg } from './styles'
 
 export function Cart() {
   return (
     <Container>
       <CartImg src={CartLogo} alt="Logo do carrinho" />
+      <CartItems />
     </Container>
   )
 }
