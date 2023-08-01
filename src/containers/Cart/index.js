10f1a1e739e824aabb21b40be63d1ee730a6de58
César <cesarsantossouza99@gmail.com>
2023-08-01 18:53:57 -0300

    add uma div

--- src/containers/Cart/index.js
+++ src/containers/Cart/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 import CartLogo from '../../assets/logo-carinho.svg'
 import { CartItems } from '../../components'
-import { Container, CartImg } from './styles'
+import { Container, CartImg, Wrapper } from './styles'
 
 export function Cart() {
   return (
     <Container>
       <CartImg src={CartLogo} alt="Logo do carrinho" />
-      <CartItems />
+      <Wrapper>
+        <CartItems />
+      </Wrapper>
     </Container>
   )
 }
